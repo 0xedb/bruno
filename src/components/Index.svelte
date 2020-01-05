31e8d0928d39bee9aab80f5dc074a93bb20c9221
theBashShell <edohbruno@gmail.com>
2020-01-05 14:48:32 +0000

    added inspiration content

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -6,6 +6,7 @@
   #index {
     margin-top: 60px;
     height: 100%;
+    font-family: "Fira Code", monospace;
   }
 
   .content {
@@ -18,11 +19,21 @@
   }
 
   .info {
-    font-size: 18px;
+    font-size: 14px;
   }
 
   .myself {
     border-radius: 100%;
+  }
+
+  .push {
+    flex-flow: column wrap;
+    max-width: 950px;
+    width: 90%;
+  }
+
+  .push-title {
+    width: 100%;
   }
 
   @media (min-width: 750px) {
@@ -34,6 +45,10 @@
       font-size: 34px;
       padding: 0px 10px;
       font-family: "Fira Code", monospace;
+    }
+
+    .info {
+      font-size: 18px;
     }
   }
 </style>
@@ -58,6 +73,24 @@
           src="/assets/img/bruno_edoh_small.png"
           alt="bruno edoh" />
       </picture>
+    </div>
+  </section>
+  <section class="flex-center">
+    <div class="push flex-center">
+      <span class="push-title">The Push</span>
+      <p>
+        My interest in computers was piqued at young age. I first learned about
+        programming at age 12, reading about the BASIC programming language from
+        the appendix of a math book. My boarding school prohibited reading
+        unapproved books, so I came very early to school when no one was around
+        and read as much as I could. I couldn’t use a computer for practice so I
+        wrote code on paper and compiled it myself. I’d solve the programming
+        questions and compare with the answers from the book. Additionally, I’d
+        give myself feedback on on every question. Around that time, I read
+        extensively about computers. Over a decade down the line, I’m happy my
+        little self made a great decision to take a career path that gives me
+        endless possibilities.
+      </p>
     </div>
   </section>
 </div>
