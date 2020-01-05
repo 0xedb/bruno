ffc11d31e3db428fd23f92d2d55ab721212337dd
theBashShell <edohbruno@gmail.com>
2020-01-05 13:52:15 +0000

    dynamic image added

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -8,15 +8,24 @@
     height: 100%;
   }
 
+  .content {
+    flex-flow: column nowrap;
+  }
+
+  .description {
+    font-size: 24px;
+    text-align: center;
+  }
+
   @media (min-width: 750px) {
     .content {
-      background: wheat;
+      flex-flow: row nowrap;
     }
 
     .description {
       font-size: 34px;
       padding: 0px 10px;
-      font-family: 'Fira Code', monospace;
+      font-family: "Fira Code", monospace;
     }
   }
 </style>
@@ -26,12 +35,17 @@
   <section class="content flex-center">
     <div class="description">
       <p>
-        Full Stack Developer &lt;-
+        Full Stack Developer
         <img src="/assets/img/verify.svg" alt="verified" />
       </p>
     </div>
     <div>
-      <img src="/assets/img/bruno_edoh_small.png" alt="bruno edoh" />
+      <picture>
+        <source
+          srcset="/assets/img/bruno_edoh_large.png"
+          media="(min-width: 750px)" />
+        <img src="/assets/img/bruno_edoh_small.png" alt="bruno edoh" />
+      </picture>
     </div>
   </section>
 </div>
