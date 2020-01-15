7b1aa51ce2bee2cdae3f61b2aa8062f0d4f03d6d
theBashShell <edohbruno@gmail.com>
2020-01-15 23:58:50 +0000

    moved global css into _layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,12 +6,28 @@
 
 <style>
   :global(html) {
+    @import url("https://fonts.googleapis.com/css?family=Caveat&display=swap");
     height: 100vh;
+    transition: all 0.5s ease;
+    scroll-behavior: smooth;
   }
-  
+  :global(.flex-center) {
+    padding-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  :global(.dark) {
+    background: #151515 !important;
+  }
+
+  :global(.light) {
+    background: unset !important;
+  }
 </style>
 
-<div id='js'>
+<div id="js">
   <main>
     <Navbar />
     <ColoMode />
