ea11df3c742ca2b8c529318f428a77a3c04a663a
theBashShell <edohbruno@gmail.com>
2020-01-17 01:42:47 +0000

    service worker structuring and writing

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,8 @@
+import { CONSTANTS } from "./util/constants";
+const filesToCache = [];
+
+const CURRENT_CACHE = CONSTANTS.cache;
+
 self.addEventListener("install", event => {
   console.log("service worker installed");
 });
@@ -8,4 +13,27 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   console.log("service worker fetch event");
+  console.log(event.request);
+
+  // event.respondWith(
+  //   caches
+  //     .match(event.request)
+  //     .then(response => {
+  //       // response || fetch(event.request);
+  //       response ? response : //
+  //     })
+  //     .catch(err => console.error(err))
+  // );
 });
+
+// function fetchAndCache(event) {
+//   const request = event.request;
+
+//   if (request.destination === "image") { 
+//     // fetch
+//     fetch(request).then(() => 
+//       caches.open(CURRENT_CACHE).then(cache => return cache.add)
+//     )
+//     // cache 
+//   }
+// }
