2531810da42c0e56087d8b0f66aafee8fe9ea645
theBashShell <edohbruno@gmail.com>
2020-01-23 21:10:39 +0000

    push fic

--- src/components/MePush.svelte
+++ src/components/MePush.svelte
@@ -1,27 +1,24 @@
 <style>
-  .push {
+  .swiper-slide {
     flex-flow: column wrap;
+  }
+
+  .push {
     max-width: 950px;
     width: 90%;
     height: 40%;
   }
 
-  .push-title {
-    width: 100%;
-    display: block;
-  }
+  
 </style>
 
 <section id="go" class="swiper-slide flex-center">
-  <div class="push flex-center">
-    <span class="push-title">The Push</span>
-    <p>
-      My interest in computers was piqued at young age. I first learned about
-      programming at age 12, reading about the BASIC programming language from
-      the appendix of a math book. Around that time, I read
-      extensively about computers. Over a decade down the line, I’m happy my
-      little self made a great decision to take a career path that gives me
-      endless possibilities.
-    </p>
-  </div>
+  <div class="push-title">The Push</div>
+  <p class="push">
+    My interest in computers was piqued at young age. I first learned about
+    programming at age 12, reading about the BASIC programming language from the
+    appendix of a math book. Around that time, I read extensively about
+    computers. Over a decade down the line, I’m happy my little self made a
+    great decision to take a career path that gives me endless possibilities.
+  </p>
 </section>
